4d7ef0334d186d13289af630b167187a0c1ff1fb
SquirrelFu <mortonmatthew8@gmail.com>
2019-02-17 16:03:27 -0500

    Updates, removing unnecessary files, some WIP stuff
    Updated build.gradle, updated the readme to no longer use the generic fabric example mod readme, removed some earlier attempts at making the appearances generate on spawn.

--- README.md
+++ README.md
@@ -1,16 +1,9 @@
-# Fabric Example Mod
+# StoryCraft
 
-## Setup
+## About the mod
 
-1. Edit build.gradle and mod.json to suit your needs.
-    * The "mixins" object can be removed from mod.json if you do not need to use mixins.
-    * Please replace all occurences of "modid" with your own mod ID - sometimes, a different string may also suffice.
-2. Run the following command:
-
-```
-./gradlew idea
-```
+This mod is an attempt to give more life and organic appearance to the NPCs of Minecraft.
 
 ## License
 
-This template is available under the CC0 license. Feel free to learn from it and incorporate it in your own projects.
+MIT License
--- src/main/java/io/github/paradoxicalblock/storycraft/main/StoryCraft.java
+++ src/main/java/io/github/paradoxicalblock/storycraft/main/StoryCraft.java
@@ -1,5 +1,13 @@
 package io.github.paradoxicalblock.storycraft.main;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.apache.commons.io.IOUtils;
+
 import io.github.paradoxicalblock.storycraft.entity.SocialVillagerFemale;
 import io.github.paradoxicalblock.storycraft.entity.SocialVillagerMale;
 import net.fabricmc.api.ModInitializer;
@@ -9,6 +17,10 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.SpawnEggItem;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.NbtIo;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.registry.Registry;
 
 public class StoryCraft implements ModInitializer {
@@ -21,6 +33,29 @@ public class StoryCraft implements ModInitializer {
 	public static final Item SOCIAL_VILLAGER_FEMALE_EGG = Registry.register(Registry.ITEM, "storycraft:social_villager_female_egg", new SpawnEggItem(SOCIAL_VILLAGER_FEMALE, 16733525, 11141120, new Item.Settings().itemGroup(ItemGroup.MISC)));
 	@Override
 	public void onInitialize() {
-
+		if (MinecraftServer.class.getClassLoader().getResource("data/minecraft/structures/village/plains/villagers/armorer.nbt") != null)
+		{			
+			CompoundTag tag = new CompoundTag();
+			ListTag entityTag = new ListTag();
+			CompoundTag nbtTag = new CompoundTag();
+			
+			try {
+				InputStream url = MinecraftServer.class.getClassLoader().getResourceAsStream("data/minecraft/structures/village/plains/villagers/armorer.nbt");
+				InputStream secondStream = MinecraftServer.class.getClassLoader().getResourceAsStream("data/minecraft/structures/village/plains/villagers/armorer.nbt");
+				tag = NbtIo.readCompressed(url);
+				entityTag = (ListTag) tag.getTag("entities");
+				nbtTag = entityTag.getCompoundTag(0);
+				nbtTag.putString("id", "storycraft:social_villager_male");
+				entityTag.add(0,nbtTag);
+				tag.put("entities", entityTag);
+				OutputStream output = Files.newOutputStream(Paths.get(""));
+				IOUtils.copy(secondStream, output);
+				NbtIo.writeCompressed(tag, output);
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
 	}
 }
